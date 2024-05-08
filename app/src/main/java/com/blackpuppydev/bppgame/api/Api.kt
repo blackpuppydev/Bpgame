46ab9c2349f8a3ab5f68629b0c8fe5221973ed62
Nattawut.c <nattawut.c@sabuytech.com>
2024-05-08 12:08:03 +0700

    commit dev

--- app/src/main/java/com/blackpuppydev/bppgame/api/Api.kt
+++ app/src/main/java/com/blackpuppydev/bppgame/api/Api.kt
@@ -1,5 +1,7 @@
 package com.blackpuppydev.bppgame.api
 
+import android.os.Build
+import com.blackpuppydev.bppgame.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,7 +13,7 @@ class Api {
 
         fun getRetrofit() : Retrofit{
             val builder: Retrofit.Builder = Retrofit.Builder()
-                .baseUrl("http://172.18.2.110:8080")
+                .baseUrl(BuildConfig.END_POINT)
                 .addConverterFactory(GsonConverterFactory.create())
 //            .client(getOkHttpClient())
             return builder.build()
